45c8cdd1b827a234da9208a1ecc4e9a0d9e0f71a
Toni Awwad <ithorn@gmail.com>
2014-09-28 17:24:00 +0200

    test

--- js/app.js
+++ js/app.js
@@ -25,9 +25,9 @@ var app = {
         // Setup the Fastclick to get rid of the click delay
         FastClick.attach(document.body);
 
-        StatusBar.show();
-        StatusBar.overlaysWebView(false);
-        StatusBar.backgroundColorByHexString("#C8DB2F");
+        //StatusBar.show();
+        //StatusBar.overlaysWebView(false);
+        //StatusBar.backgroundColorByHexString("#C8DB2F");
 
         // Push body if iOS version gt 7
         //if (parseFloat(device.version) >= 7.0) {
